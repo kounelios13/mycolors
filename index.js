e2f69e3e81869c07044d366ed2e4e2df6951832f
kounelios13 <pkounelios@gmail.com>
2016-12-18 14:52:34 +0200

    fixed script

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-var colors = ["#0091d8","#00acd8","#ff8541","#ffe541","#4dc4ff","#e40026"];
-function createSamples(outputPanel){
-	var samples = "<div class='row'>";
-	for(var i = 0,max = colors.length; i < max; i++){
-		var color = colors[i];
-		samples+="<div class='col-md-3'><div class = 'sample' style='background-color:"+color+"'></div></div>";
-		if(i % 4 == 0 && i>0)
-			samples+="</div><div class='row'>";
+var colors = ["#0091d8", "#00acd8", "#ff8541", "#ffe541", "#4dc4ff", "#e40026"];
+var createList = function(colorString) {
+	var array = colorString.split(",");
+	var max = array.length;
+	var list = "<div class='list-group'>";
+	for (var i = 0; i < max; i++) {
+		var color = array[i];
+		list += "<div class='list-group-item' style='background-color:" + color + "'>" + color + "</div>";
 	}
-
-	samples+="</div>";
+	list += "</div>";
+	return list;
+};
+function createSamples(outputPanel, colorlist) {
+	var samples = "<div class='container-fluid'><div class='row'>";
+	var i = 0;
+	var max = colorlist.length;
+	for (; i < max; i++) {
+		if (i > 0 && i % 4 == 0)
+			samples += "</div><div class='row'>";
+		samples += "<div class='sample col-md-3' style='background-color:" + colorlist[i] + "'></div>";
+	}
+	samples += "</div></div>";
+	samples += "";
 	$(outputPanel).html(samples);
 }
-$(function(){
-	createSamples("#colorexamples");
+$(function() {
+	createSamples("#colorexamples", colors);
 });
