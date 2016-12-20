3a4443b73ac197957f77eb5af9931aa4be33dc2c
kounelios13 <pkounelios@gmail.com>
2016-12-20 20:17:52 +0200

    fixes #3

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ var colors = ["#e22d00","#2f688e","#6ed3cf","#9ad3de",
 "#0091d8", "#00acd8", "#ff8541", "#ffe541",
  "#4dc4ff", "#e40026","#98dafc","#6534ff",
  "#62bcfa","#e05038","#c43235","#f2b632",
- "#D32F2F","#F44336","#FF5722"];
-/*var t = colors.slice(0);
-for(var r of colors)
-	t.push(r);*/
+ "#D32F2F","#F44336","#FF5722","#FFC107",
+ "#1bbfe4","#E71D36","#004BA8","#FF9F1C"];
 var createList = function(colorString) {
 	var array = colorString.split(',');
 	var max = array.length;
@@ -45,16 +43,24 @@ var createColorLists = function(colorList){
 		}
 		rowIndex+=6;
 		var colorString = colorList.slice(colorIndex,colorIndex+4).join(",");
-		colorIndex+=4;
+
 		var ul = createList(colorString);
-		var wrapped = wrapToCol(ul,remaining==1&& i >= totalLists?12:6);
+		// fixes #3
+		var wrapped = wrapToCol(ul,mod(arraySize,4) && i==totalLists-1?12:6);
 		str+=wrapped;
+		colorIndex+=4;
 	}
 	str+="</div></div>";
 	return str;
 };
 $(function() {
+	var array = Array.from(new Set(colors)).sort();
+	var max = array.length;
+	var pos = Math.floor(Math.random() * max);
+	var cur = array[pos];
+	log(cur)
 	var markup = createColorLists(Array.from(new Set(colors)).sort());
+	$("body").css("background", cur);
 	$("#colorexamples").html(markup);
 	$(".list-group-item").on("click", function() {
 		var color = $(this).html();
